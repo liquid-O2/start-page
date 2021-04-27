b438121a4f8135349b9de29fa541ffd31d463987
liquid-O2 <this.is.arunava.b@gmail.com>
2021-04-27 15:10:59 +0530

    added the basic start page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="box">
+    <i class="fas fa-code"> <span> | sudo start</span></i>
+
+    <a href="https://github.com/liquid-O2">Github</a>
+    <a href="https://www.udemy.com/">Udemy</a>
+    <a href="https://vuesax.com/docs/theme/#default-colors">Vuesax</a>
+    <a href="https://v3.vuejs.org/guide/introduction.html">Vue</a>
+    <a
+      href="https://fontawesome.com/icons?d=gallery&p=2
+"
+      >FontAwesome</a
+    >
+    <a href="https://undraw.co/illustrations">Undraw</a>
+    <a href="https://www.youtube.com/">Youtube</a>
+    <a href="https://docs.google.com/document/u/0/">Docs</a>
+    <i class="fas fa-terminal">
+      <span> // continue your coding journey </span></i
+    >
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Space+Mono&display=swap");
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Space Mono", monospace;
+}
+body {
+  background-color: #2e3440;
+  color: #e5e9f0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.box {
+  background: #3b4252;
+  border-radius: 0.75rem;
+  width: fit-content;
+  height: fit-content;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  padding: 2rem;
+  flex: 0 1 30rem;
+}
+a {
+  text-decoration: none;
+  color: #e5e9f0;
+  padding: 0.4rem 0rem;
+}
+i {
+  padding: 1rem 0rem;
+  font-size: 1.2rem;
+  color: #8fbcbb;
+}
+i > span {
+  font-family: "Space Mono", monospace;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+// library.add(faUserSecret);
 
-createApp(App).mount('#app')
+// Vue.component("font-awesome-icon", FontAwesomeIcon);
+createApp(App).mount("#app");
